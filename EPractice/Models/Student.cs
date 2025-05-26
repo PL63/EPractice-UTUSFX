b63bd39ebc54bc3d91638377353a35c161c907c6
PL63 <montecinos.p@outlook.com>
2025-05-25 22:21:28 -0400

    Update Student.cs

---
+++ EPractice/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace EPractice.Models;
+
+{
+    public class Student
+    {
+        public int CI { get; set; }
+        public string Nombre { get; set; }
+        public int Nota { get; set; }
+    }
+
+}
